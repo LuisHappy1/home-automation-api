506b655dc2ec0240b5b0456024a5cfefb98e03a6
Luis Camacho <lcamacho@iastate.edu>
2020-11-29 19:54:29 -0600

    Added more options

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
-from rpi_rf import RFDevice
-import time
+import json
+
+from flask import Flask, render_template, request
+
+from static.actions.outlet_transmitter import send_code
+
 app = Flask(__name__)
 
-rfdevice = RFDevice(17)
-rfdevice.enable_tx()
-rfdevice.tx_repeat = 10
+outletCodes = {
+    "lamp": {
+        "on": {
+            "code": 9407996,
+            "protocol": 1,
+            "pulse_length": 163
+        },
+        "off": {
+            "code": 9407988,
+            "protocol": 3,
+            "pulse_length": 71
+        }
+    },
+    "christmas_tree": {
+        "on": {
+            "code": 9407994,
+            "protocol": 1,
+            "pulse_length": 185
+        },
+        "off": {
+            "code": 9407986,
+            "protocol": 1,
+            "pulse_length": 170
+        }
+    }
+}
 
 
 @app.route('/')
@@ -15,28 +41,24 @@ def home():
     return render_template('home.html')
 
 
-# on
-# python3 rfrx_send.py 9407996 -p 163 -t 1
-# off
-# python3 rfrx_send.py 9407988 -p 71 -t 3
-@app.route('/lampOn')
-def lamp_on():
-    print('lamp will be turned on')
-    rfdevice.tx_code(9407988, 3, 71, 24)  # Off
-    time.sleep(.5)
-    rfdevice.tx_code(9407996, 1, 163, 24)  # On
+@app.route('/handle-button', methods=['POST'])
+def handle_button():
+    data = json.loads(request.data)
+    current_outlet = outletCodes[data['name']][data['lightSetting']]
+
+    send_code(current_outlet['code'], current_outlet['protocol'], current_outlet['pulse_length'])
+
     return render_template('home.html')
 
 
-@app.route('/lampOff')
-def lamp_off():
-    print('lamp will be turned off')
-    rfdevice.tx_code(9407996, 1, 163, 24)
-    time.sleep(.5)
-    rfdevice.tx_code(9407988, 3, 71, 24)
+@app.route('/turn-off-all-lights', methods=['GET'])
+def turn_all_lights_off():
+    for outlet in outletCodes:
+        current_outlet = outletCodes[outlet]['off']
+        send_code(current_outlet['code'], current_outlet['protocol'], current_outlet['pulse_length'])
+
     return render_template('home.html')
 
 
 if __name__ == "__main__":
-    print('here in the main')
     app.run(host='0.0.0.0')
